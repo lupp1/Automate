63c753118587919d84df0de2a1281e7ebf1a5b11
Gustavo <gugas_soares@hotmail.com>
2021-09-01 16:56:26 -0300

    Add projects

---
+++ Test Projects/collatzSequence.py
@@ -0,0 +1,18 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print((3 * number) + 1)
+        return (3 * number) + 1
+try:
+    n = input ('Enter a number: ')
+    while n != 1:
+        n = collatz(int(n))
+except ValueError:
+    print('Enter an integer value.')
+    pass
+
+
+
+
