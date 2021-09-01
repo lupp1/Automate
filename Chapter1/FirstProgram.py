1488f2a12cd88556d0fbed3d3d64943c6760a6ed
Gustavo <gugas_soares@hotmail.com>
2021-09-01 16:47:30 -0300

    Update FirstProgram.py

---
+++ Chapter1/FirstProgram.py
@@ -0,0 +1,11 @@
+#Changed 
+
+print('Hello, world!')
+print('What is your name?') #Ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The lenght of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print(f'You will be {str(int(myAge) + 1)} in a year')
