7882ae4037448b395096405d4210ebe8c1235f4a
Gustavo <gugas_soares@hotmail.com>
2021-09-01 18:31:37 -0300

    Add commaCode.py

--- Test Projects/commaCode.py
+++ Test Projects/commaCode.py
@@ -1,2 +1,14 @@
-def commaFunc(listValue):
-    
+def commafunc(listValue):
+    for i in range(len(listValue) - 2):
+        print(listValue[i], end=', ')
+    print(f'{listValue[-2]} and {listValue[-1]}')
+
+def generateList():
+    spam = []
+    n = int(input('Enter the number of elements for the list: '))
+    for i in range(0, n):
+        element = input(f'Type element number {i}: ')
+        spam.append(element)
+    return spam
+spam = generateList()
+commafunc(spam)
