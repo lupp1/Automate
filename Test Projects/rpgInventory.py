50fc7b7540d5c4d47164711ed234282ef7d9c5d6
Gustavo <gugas_soares@hotmail.com>
2021-09-09 08:56:52 -0300

    Add picnicTime.py

--- Test Projects/rpgInventory.py
+++ Test Projects/rpgInventory.py
@@ -1,4 +1,4 @@
-#Programa que mostar o total de itens em uma mochila
+#Programa que mostra o total de itens em uma mochila
 inventory = {'Flechas': 15, 'Pacote de Aventureiro': 1, 'Corda': 2, 'Adaga': 1,
              'Moedas': 100, 'Poção de vida': 3}
 def displayInventory(bagOfStuff):
