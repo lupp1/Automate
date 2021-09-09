076c9a1e03ea2429633df95ffceaa2bc2e881c37
Gustavo <gugas_soares@hotmail.com>
2021-09-09 08:56:06 -0300

    Add rpgInventory.py

---
+++ Test Projects/rpgInventory.py
@@ -0,0 +1,11 @@
+#Programa que mostar o total de itens em uma mochila
+inventory = {'Flechas': 15, 'Pacote de Aventureiro': 1, 'Corda': 2, 'Adaga': 1,
+             'Moedas': 100, 'Poção de vida': 3}
+def displayInventory(bagOfStuff):
+    print('Seu inventário é: ')
+    totalItens = 0
+    for k, v in inventory.items():
+        print(f'{v} {k}')
+        totalItens += v
+    print(f'O total de itens na mochila é: {totalItens}')
+displayInventory(inventory)
