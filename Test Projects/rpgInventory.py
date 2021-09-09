cbe8992c06126a55c8fff1db2e0ff97f76bfce9a
Gustavo <gugas_soares@hotmail.com>
2021-09-09 10:06:08 -0300

    Add rpgInventory.py and extra function

--- Test Projects/rpgInventory.py
+++ Test Projects/rpgInventory.py
@@ -1,6 +1,8 @@
 #Programa que mostra o total de itens em uma mochila
-inventory = {'Flechas': 15, 'Pacote de Aventureiro': 1, 'Corda': 2, 'Adaga': 1,
-             'Moedas': 100, 'Poção de vida': 3}
+def addToInventory(bagOfStuff, addedItems):
+    for i in range(len(addedItems)):
+        bagOfStuff.setdefault(addedItems[i], 0)
+        bagOfStuff[addedItems[i]] = bagOfStuff[addedItems[i]] + 1
 def displayInventory(bagOfStuff):
     print('Seu inventário é: ')
     totalItens = 0
@@ -8,4 +10,10 @@ def displayInventory(bagOfStuff):
         print(f'{v} {k}')
         totalItens += v
     print(f'O total de itens na mochila é: {totalItens}')
+
+inventory = {'Flechas': 15, 'Pacote de Aventureiro': 1, 'Corda': 2, 'Adaga': 1,
+             'Moedas': 100, 'Poção de vida': 3}
+dragonLoot = ['Moedas', 'Moedas', 'Moedas', 'Rubi', 'Osso', 'Osso']
+newInv = addToInventory(inventory, dragonLoot)
 displayInventory(inventory)
+
