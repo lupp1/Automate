1e711a2306ecb29046bc0e3c3b93fa534022a42c
Gustavo <gugas_soares@hotmail.com>
2021-10-02 17:02:55 -0300

    Add Chapter6 dir

---
+++ Chapter6/catnapping.py
@@ -0,0 +1,9 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary and extortion.
+
+Sincerely, 
+Bob''')
+#Compared to
+print('\nCompared to:')
+print('\nDear Alice, \n\nEve\'s cat has been arrested for catnapping, cat burglary and extortion.\n\nSincerely,\nBob')
---
+++ Chapter6/test.py
@@ -0,0 +1,13 @@
+"""Test program in Python, actually testing the multiline comment feature.
+I hope you understand.
+
+This program was design for Python 3.8.1!
+Gustavo."""
+
+def spam():
+    """This is a multiline comment to help
+    explain what the function spam() does.
+    It prints the string 'Hello!'
+    """
+    print('Hello!')
+spam()
