472eedc6526944f8fa62d2c419c2ab8c00da6bc7
Gustavo <gugas_soares@hotmail.com>
2022-01-06 22:12:36 -0300

    added files

---
+++ Chapter10/tracebackUsage.py
@@ -0,0 +1,9 @@
+import traceback
+
+try:
+    raise Exception('This is the error message.')
+except:
+    with open('errorInfo.txt', 'w') as errorFile:
+        log = errorFile.write(traceback.format_exc())
+        print('The traceback was written to errorInfo.txt.')
+        
